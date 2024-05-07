3fee27dd1b6d480e15bf6c7b614ab2dd0db16a57
kedd <kedd@allexceed.co.jp>
2024-05-07 12:22:42 +0900

    add antd5

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -4,6 +4,7 @@
 const React = require('react'); // <1>
 const ReactDOM = require('react-dom'); // <2>
 const client = require('./client'); // <3>
+import { Button } from 'antd';
 // end::vars[]
 
 // tag::app[]
@@ -23,7 +24,8 @@ class App extends React.Component { // <1>
 	render() { // <3>
 		return (
 			<div>
-				test
+				<h1>Hello Ant Design 5</h1>
+				<Button type="primary">Primary Button</Button>
 			</div>
 		)
 	}
