67333b876d391f44b77d5d6661ef00cbfeba9663
kedd <kedd@allexceed.co.jp>
2024-05-30 17:54:10 +0900

    sample commit

---
+++ src/main/java/com/alxvn/excel/MyApplication.java
@@ -0,0 +1,12 @@
+package com.alxvn.excel;
+
+import com.alxvn.excel.app.JavaFxApplication;
+
+import javafx.application.Application;
+
+public class MyApplication {
+
+	public static void main(String[] args) {
+		Application.launch(JavaFxApplication.class, args);
+	}
+}
